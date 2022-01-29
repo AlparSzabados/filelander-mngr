fc7c441383e71c58cd1bb40d43603d756ca0b6bf
Alpar <szabadosalpar@gmail.com>
2022-01-29 17:04:43 +0200

    Initial commit

---
+++ app/src/main/java/org/filelandermngr/FilelanderMngrApplication.java
@@ -0,0 +1,11 @@
+package org.filelandermngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilelanderMngrApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FilelanderMngrApplication.class, args);
+	}
+}
---
+++ app/src/test/java/org/filelandermngr/scanner/DummyTest.java
@@ -0,0 +1,15 @@
+package org.filelandermngr.scanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DummyTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
